98fa36498ff81d94d4611e5279f34ba586ae4c6a
Santana <matheus.santana@sap.com>
2022-06-06 23:16:58 -0300

    add login

---
+++ app/src/main/java/com/example/projeto_final_cdm/TrackStartStop/view/FormLogin.java
@@ -0,0 +1,27 @@
+package com.example.projeto_final_cdm.TrackStartStop.view;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.projeto_final_cdm.R;
+
+public class FormLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_login);
+
+        //getSupportActionBar().hide();
+    }
+
+    public void goToMain(View view) {
+        Intent intent = new Intent(FormLogin.this,MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
--- app/src/main/java/com/example/projeto_final_cdm/TrackStartStop/view/MainActivity.java
+++ app/src/main/java/com/example/projeto_final_cdm/TrackStartStop/view/MainActivity.java
@@ -1,24 +1,15 @@
 package com.example.projeto_final_cdm.TrackStartStop.view;
 
-import static android.provider.AlarmClock.EXTRA_MESSAGE;
-
-import static com.example.projeto_final_cdm.R.id.MainDrawer;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.content.Intent;
-import android.content.pm.LabeledIntent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import com.example.projeto_final_cdm.R;
-import com.example.projeto_final_cdm.Sensores.GPSActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,25 +21,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // drawer layout instance to toggle the menu icon to open
-        // drawer and back button to close drawer
         drawerLayout = findViewById(R.id.MainDrawer);
         actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
 
-        // pass the Open and Close toggle for the drawer layout listener
-        // to toggle the button
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
 
-        // to make the Navigation drawer icon always appear on the action bar
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
-    // override the onOptionsItemSelected()
-    // function to implement
-    // the item click listener callback
-    // to open and close the navigation
-    // drawer when the icon is clicked
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
