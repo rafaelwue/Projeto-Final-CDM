debb8015b09943c7437ca5f610ba11736fbb8c14
Santana <matheus.santana@sap.com>
2022-06-26 13:00:37 -0300

    add recyclerview

--- app/src/main/java/com/example/projeto_final_cdm/Aplicativo/ViewHolders/HistoricoViewHolder.java
+++ app/src/main/java/com/example/projeto_final_cdm/Aplicativo/ViewHolders/HistoricoViewHolder.java
@@ -8,16 +8,18 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.projeto_final_cdm.R;
 
 public class HistoricoViewHolder extends RecyclerView.ViewHolder {
+    public TextView id_view;
     public TextView latitude_view;
     public TextView longitude_view;
-    public TextView time;
-    public TextView enviado;
+    public TextView time_view;
+    public TextView enviado_view;
 
     public HistoricoViewHolder(View view){
         super(view);
+        id_view = (TextView) view.findViewById(R.id.id_view);
         latitude_view = (TextView) view.findViewById(R.id.latitude_view);
         longitude_view = (TextView) view.findViewById(R.id.longitude_view);
-        time = (TextView) view.findViewById(R.id.time);
-        enviado = (TextView) view.findViewById(R.id.enviado);
+        time_view = (TextView) view.findViewById(R.id.time_view);
+        enviado_view = (TextView) view.findViewById(R.id.enviado_view);
     }
 }
--- app/src/main/java/com/example/projeto_final_cdm/Aplicativo/view/LocationActivity.java
+++ app/src/main/java/com/example/projeto_final_cdm/Aplicativo/view/LocationActivity.java
@@ -9,6 +9,7 @@ import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
@@ -88,5 +89,11 @@ public class LocationActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    public void goToHistorico(View view) {
+        Intent intent = new Intent(LocationActivity.this, HistoricoPosActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
